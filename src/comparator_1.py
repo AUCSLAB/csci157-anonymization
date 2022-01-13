83c267f177b46152c8079b7b68274a1b120033e9
gareging <mr.garegin.grigoryan@gmail.com>
2022-01-12 21:52:38 -0500

    Update comparator_1.py

--- src/comparator_1.py
+++ src/comparator_1.py
@@ -1,11 +1,10 @@
- '''
- Copyright 2021 Garegin Grigoryan (grigoryan@alfred.edu)
-
- This work is licensed under the Creative Commons
- Attribution-NonCommercial-ShareAlike 4.0 International License.
- To view a copy of this license, visit
- http://creativecommons.org/licenses/by-nc-sa/4.0/.
- '''
+'''
+Copyright 2021 Garegin Grigoryan (grigoryan@alfred.edu)
+This work is licensed under the Creative Commons
+Attribution-NonCommercial-ShareAlike 4.0 International License.
+To view a copy of this license, visit
+http://creativecommons.org/licenses/by-nc-sa/4.0/.
+'''
 
 import sys
 
